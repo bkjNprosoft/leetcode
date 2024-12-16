aa4cb6a33a4c14ffec7ddea1b515d7d669fbf3ed
bkjNprosoft <bookj.nprosoft@gmail.com>
2024-12-16 18:13:36 +0900

    add 145

--- 145.binary-tree-postorder-traversal.js
+++ 145.binary-tree-postorder-traversal.js
@@ -1,24 +1,25 @@
 /*
- * @lc app=leetcode id=144 lang=javascript
+ * @lc app=leetcode id=145 lang=javascript
  *
- * [144] Binary Tree Preorder Traversal
+ * [145] Binary Tree Postorder Traversal
  */
 
 // @lc code=start
 /** Definition for a binary tree node. */
 function TreeNode(val, left, right) {
-    this.val = (val===undefined ? 0 : val)
-    this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
 }
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
-const preorderTraversal = function(root) {
+const postorderTraversal = function(root) {
   const result = [];
 
-  // 루트 노드부터 자식노드를 전위 탐색(Preorder)하여 배열에 데이터를 추가한다.
+  // 루트 노드부터 자식 노드를 후위 탐색(postodrer)하여 배열에 데이터를 추가한다.
   traversal(root, result);
 
   return result;
@@ -29,9 +30,7 @@ const preorderTraversal = function(root) {
  * @param {Array} array
  */
 function traversal(node, array) {
-  if (!node) {
-    return;
-  }
+  if (!node) return;
 
   if (!node.left && !node.right) {
     // 자식 노드가 없는 잎 노드(단말 노드)인 경우 탐색을 끝내고 데이터를 추가
@@ -39,11 +38,10 @@ function traversal(node, array) {
     return;
   }
 
-  array.push(node.val);
-
-  // 전위 순회 순서인 left -> root -> right 순으로 탐색한다.
+  // 후위 순회 순서인 left -> right -> root 순으로 탐색한다.
   traversal(node.left, array);
   traversal(node.right, array);
+  array.push(node.val);
 }
 // @lc code=end
 
